d9b593800abf934109b3a44a16d333d507250844
Andrea <andrea.doreste@poli.ufrj.br>
2018-11-27 02:11:16 -0200

    this version needs another cloudservice

--- src/org/eclipse/kura/stormseeker/tempsubscriber/TemperatureSubscriber.java
+++ src/org/eclipse/kura/stormseeker/tempsubscriber/TemperatureSubscriber.java
@@ -1,5 +1,6 @@
 package org.eclipse.kura.stormseeker.tempsubscriber;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -12,10 +13,21 @@ import org.eclipse.kura.cloudconnection.message.KuraMessage;
 import org.eclipse.kura.cloudconnection.subscriber.CloudSubscriber;
 import org.eclipse.kura.cloudconnection.subscriber.listener.CloudSubscriberListener;
 import org.eclipse.kura.configuration.ConfigurableComponent;
+import org.eclipse.kura.gpio.GPIOService;
+import org.eclipse.kura.gpio.KuraClosedDeviceException;
+//import org.eclipse.kura.gpio.KuraClosedDeviceException;
+import org.eclipse.kura.gpio.KuraGPIODeviceException;
+import org.eclipse.kura.gpio.KuraGPIOPin;
+import org.eclipse.kura.gpio.KuraGPIOTrigger;
+import org.eclipse.kura.gpio.KuraGPIOMode;
+import org.eclipse.kura.gpio.KuraGPIODirection;
+import org.eclipse.kura.gpio.KuraUnavailableDeviceException;
 import org.eclipse.kura.message.KuraPayload;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 public class TemperatureSubscriber implements ConfigurableComponent, CloudConnectionListener, CloudSubscriberListener {
 
@@ -26,14 +38,37 @@ public class TemperatureSubscriber implements ConfigurableComponent, CloudConnec
 	private CloudSubscriber cloudSubscriber;
 	private final ScheduledExecutorService worker;
 	private ScheduledFuture<?> handle;
+	
+	private boolean tempLedStatus;
+	private boolean humLedStatus;
+
+	//GPIO Variables
+	private GPIOService gpioService;
+	//private GPIOService gpioServiceGreen;
+	KuraGPIOPin ledRed;
+	KuraGPIOPin ledGreen;
+	//KuraGPIOPin led;
+	//this.gpioService.getPinByTerminal(26);
 
 	//Constructor
-	public TemperatureSubscriber() {
+	public TemperatureSubscriber() throws KuraGPIODeviceException, KuraUnavailableDeviceException, IOException {
 		super();
 		this.worker = Executors.newSingleThreadScheduledExecutor();
+		
+		//led.open();
+				
+	}
+	
+	//GPIO methods
+	public void setGPIOService(GPIOService gpioService) {
+		this.gpioService = gpioService;
+	}
+	
+	public void unsetGPIOService(GPIOService gpioService) {
+		gpioService = null;
 	}
 
-	//Set and Unset methods
+	//Set and Unset subscriber methods
 	public void setCloudSubscriber(CloudSubscriber cloudSubscriber) {
 		this.cloudSubscriber = cloudSubscriber;
 		this.cloudSubscriber.registerCloudSubscriberListener(TemperatureSubscriber.this);
@@ -47,7 +82,7 @@ public class TemperatureSubscriber implements ConfigurableComponent, CloudConnec
 	}
 
 	//Activation API
-	protected void activate(ComponentContext componentContext, Map<String, Object> properties) {
+	protected void activate(ComponentContext componentContext, Map<String, Object> properties) throws KuraClosedDeviceException {
 		s_logger.info("Temp Sub has started with config!");
 		//updated(properties);
 		this.properties = properties;
@@ -57,11 +92,49 @@ public class TemperatureSubscriber implements ConfigurableComponent, CloudConnec
 		}
 
 		s_logger.info("Activating Temp... Done.");
+	
+		//GPIO
+		//KuraGPIOPin pin = gpioService.getPinByTerminal(12);
+		//KuraGPIOPin customInputPin = gpioService.getPinByTerminal(26, KuraGPIODirection.INPUT,KuraGPIOMode.INPUT_PULL_UP,KuraGPIOTrigger.BOTH_LEVELS);
+		this.ledRed = gpioService.getPinByTerminal(12);
+		this.ledGreen = gpioService.getPinByTerminal(26);
+		
+		try {
+			if(this.ledRed.isOpen()==true) {
+				this.ledRed.close();
+			}
+			if(this.ledGreen.isOpen()==true) {
+				this.ledGreen.close();
+			}
+			//this.ledRed.open();
+			//this.ledGreen.open();
+			//trying to test the pin
+			//int a = 0;
+			//while (a<10) {
+			this.ledRed.setValue(true);
+			this.ledGreen.setValue(true);
+				//pin.setValue(false);
+				//a= a +1;
+				
+			//customInputPin.open();
+		} catch (KuraUnavailableDeviceException e2) {
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+		
+		s_logger.info("gpio has started");
+
 	}
 
-	protected void deactivate(ComponentContext componentContext) {
+	protected void deactivate(ComponentContext componentContext) throws KuraUnavailableDeviceException, KuraClosedDeviceException, IOException {
 		s_logger.info("Tem Sub has stopped!");
 		this.worker.shutdown();
+		this.ledRed.setValue(false);
+		this.ledRed.close();
+		
+		this.ledGreen.setValue(false);
+		this.ledGreen.close();
 	}
 
 	public void updated(Map<String, Object> properties) {
@@ -108,10 +181,59 @@ public class TemperatureSubscriber implements ConfigurableComponent, CloudConnec
 		}
 
 		if (payload.metrics() != null) {
+			String name = null; //will be 'temperature' or 'humidity'
+			String status = "false";
 			for (Entry<String, Object> entry : payload.metrics().entrySet()) {
 				s_logger.info("Message metric: {}, value: {}", entry.getKey(), entry.getValue());
+				
+				if(entry.getKey().equals("name")) {
+					name = entry.getValue().toString();
+				}
+				
+				if(entry.getKey().equals("status")) {
+					status = entry.getValue().toString();
+				}
 			}
+			if(name.equals("temperature")){
+				this.tempLedStatus = Boolean.parseBoolean(status); 
+			} 
+			if(name.equals("humidity")) {
+				this.humLedStatus = Boolean.parseBoolean(status);
+			}
+				
+			try {
+				check_threshold();
+			} catch (KuraUnavailableDeviceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (KuraClosedDeviceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
+	}
+	
+	
+	public void check_threshold() throws KuraUnavailableDeviceException, KuraClosedDeviceException, IOException {
+		
+		if((tempLedStatus==true)&(humLedStatus==true)) {
+			this.ledRed.setValue(true);
+			this.ledGreen.setValue(true);
+		} else if ((tempLedStatus==true)&(humLedStatus==false)) {
+			this.ledRed.setValue(true);
+			this.ledGreen.setValue(false);
+		} else if ((tempLedStatus==false)&(humLedStatus==true)) {
+			this.ledRed.setValue(false);
+			this.ledGreen.setValue(true);
+		} else {
+			this.ledRed.setValue(false);
+			this.ledGreen.setValue(false);
+		}
+		
 	}
 
 }
